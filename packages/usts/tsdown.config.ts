abbd875e3276ca436f030d9d25f43aece4b38260
Roman <dev@rman.dev>
2026-01-17 14:27:19 +0000

    Update dependencies

--- packages/usts/tsdown.config.ts
+++ packages/usts/tsdown.config.ts
@@ -1,21 +1,30 @@
-import { defineConfig } from "tsdown";
+import { defineConfig, type UserConfig } from "tsdown";
+
+function defineEntryPoint({
+  entry,
+  dts = true,
+}: {
+  entry: Record<string, string>;
+  dts?: boolean;
+}) {
+  return {
+    entry,
+    exports: true,
+    hash: false,
+    outputOptions: { minify: "dce-only" },
+    dts,
+  } satisfies UserConfig;
+}
 
 export default defineConfig([
-  {
+  defineEntryPoint({
     entry: {
       index: "src/config/index.ts",
       config: "src/config/index.ts",
     },
-    exports: true,
-    hash: false,
-    outputOptions: { minify: "dce-only" },
-    dts: true,
-  },
-  {
+  }),
+  defineEntryPoint({
     entry: { "bin/cli": "src/bin/cli.ts" },
-    exports: true,
-    hash: false,
     dts: false,
-    outputOptions: { minify: "dce-only" },
-  },
+  }),
 ]);
